99641c9ea7c880079320e641ae30d6a85d959f3e
HolidayChu <zwyai1994@163.com>
2016-12-07 11:32:32 +0800

    Update MainActivity.java

--- src/com/example/github/MainActivity.java
+++ src/com/example/github/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //12345上山打老虎
     }
 
 
